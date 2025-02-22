78c5901d54dce120c050baa5bc8d2afbc93edbda
Jahongir <alimovj727@gmail.com>
2025-02-22 11:51:03 +0500

    Route to delete only the authenticated user's own post.

--- routes/api.php
+++ routes/api.php
@@ -15,4 +15,4 @@ Route::get('/posts', [PostController::class, 'index']);
 Route::post('/posts', [PostController::class, 'store']);
 Route::get('/posts/{id}', [PostController::class, 'show']);
 Route::put('/posts/{id}', [PostController::class, 'update']);
-
+Route::delete('/posts/{id}', [PostController::class, 'destroy']);
